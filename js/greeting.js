3401b7ebefc122e31a34a6df86ba843bcfa7f485
Mikhail Dhruv <mikhail@Mikhails-MacBook-Pro.local>
2016-04-05 23:21:14 -0700

    Read Greetings from spreadsheet.

---
+++ js/greeting.js
@@ -0,0 +1,26 @@
+var gretting_key = '1bWKEX2i2KeVBxGNDZsA90rQ25U0L8G9mfRwpxcKBJQg';
+
+var updateGreeting = function () {
+  Tabletop.init( { key: gretting_key,
+                   callback: showInfo,
+                   simpleSheet: true } )
+};
+
+function showInfo(data, tabletop) {
+  var greetings = [];
+  for(i = 0; i < data.length; i++) {
+    for(prop in data[i]) {
+	  greetings.push(data[i][prop])
+    }
+  }
+  
+  html = '<p id="greeting">'
+  html += greetings[Math.floor(Math.random() * greetings.length)];
+  html += '</p>'
+  $("#greeting").html(html);
+}
+
+$(document).ready(function() {
+	updateGreeting()
+    setInterval(updateGreeting, 21600000);
+});
