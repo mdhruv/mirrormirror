bcb1776b9386f84af957f636295f4487f8c7b122
Mikhail Dhruv <mikhail@Mikhails-MacBook-Pro.local>
2019-02-06 19:43:53 -0800

    Update date to be two lines so it doesn't make weather widget mess up.

--- js/clock.js
+++ js/clock.js
@@ -3,7 +3,7 @@ date = null;
 
 var updateTime = function () {
     date = moment(new Date())
-    var html = '<p>' + date.format('dddd, MMMM Do') + '</p>';
+    var html = '<p>' + date.format('dddd') +'<br>'+ date.format('MMMM Do')+ '</p>';
     html += '<p id="time">' + date.format('h:mm a') + '</p>';
     datetime.html(html);
 };
